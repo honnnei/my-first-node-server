cad8a1ab3382eaddbb7669d55a8616837f18f3f3
honnnei <hannpop@gmail.com>
2020-03-26 17:48:32 +0100

    hussayn's code + README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # my-first-node-server
 ## Hanna & Matt
+### What I/we have learned
+- how to run a node server
+- how to take input from the url 
+- how to link to unsplush API using accesskey
+ 
+### What challenges I/we overcame
+- managed to target the desired info from the API 
+- managed to display an image from an API through the url-user input  (parse(url))
+
+### What I/we struggled with
+- getting the server to activate the javascript & HTML interaction
+
+### What I/we would do differently next time
--- main.js
+++ main.js
@@ -1,59 +1,35 @@
 var http = require('http');
-var url = require('url');
-//now calling that module:
-//important: const fs = require(fs);
-
-
-
-http.createServer(function(req, res){
-    const q = url.parse(req.url, true).query;
-    console.log(q);
-    const search_keywords = q.search;
-    //console.log(req); //-> shows millions of line in terminal
-    //console.log(req.url); 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    //res.end('Hello World!');
-    
-
-    unsplash.search.photos(search_keywords, 1, 10, { orientation: "portrait" })
-    .then(toJson)
-    .then(json => {
-      // Your code
-      imgUrl = json.results[0].urls.raw;
-      console.log(imgUrl);
-      res.end("<img style=\"width:500px;height:600px;\" src= " + imgUrl + ">");
-    });
-
-    
-
-}).listen(8080);
-
-// let url = "https://api.github.com/users/honnnei";
-// fetch(url).then(function(response) { return response.json(); }).then(function(myJson) { console.log(JSON.stringify(myJSON)); });
-
-// require syntax
+const fs = require('fs');
 const fetch = require('node-fetch');
 global.fetch = fetch;
 
-const Unsplash = require('unsplash-js').default;
-
-const toJson = require('unsplash-js').toJson;
- 
-// const unsplash = new Unsplash({ accessKey: "Kq5yEtAw5yUEUGweVBPDUm22CBLvlf-6_lVHrveyo78" });
- 
-const unsplash = new Unsplash({
-  accessKey: "Kq5yEtAw5yUEUGweVBPDUm22CBLvlf-6_lVHrveyo78",
-  // Optionally you can also configure a custom header to be sent with every request
-  headers: {
-    "X-Custom-Header": "foo"
-  },
-  // Optionally if using a node-fetch polyfill or a version of fetch which supports the timeout option, you can configure the request timeout for all requests
-  timeout: 500 // values set in ms
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8080);
 });
 
 
-
-
-
-
-
+function getImages() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(fetch("https://source.unsplash.com/random"))
+        }, 3000);
+    });
+}
+async function getURLs(){
+    const a = await getImages().then(result => result.url);
+    const b = await getImages().then(result => result.url);
+    console.log(a)
+    displayImages(a,b)
+}
+const displayImages = (x,y) => {
+    document.getElementById("image1").innerHTML = `<img src="${x}">`
+    document.getElementById("image2").innerHTML = `<img src="${y}">`
+};
+getURLs()
